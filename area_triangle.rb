7c46f04aca1b68869c1ff7c75a9f8119c4b42242
Kirill Ustimenko <kirillystim@gmail.com>
2019-06-27 21:50:43 +0300

    2 task

---
+++ area_triangle.rb
@@ -0,0 +1,9 @@
+puts "Укажите длину основания треугольника (см.): "
+a_triangle = gets.to_i
+
+puts "Укажите высоту треугольника (см.): "
+h_triangle = gets.to_i
+
+s_triangle = (a_triangle*h_triangle)/2
+
+puts "Площадь треугольника составляет: #{s_triangle} см^2"
